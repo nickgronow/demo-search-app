4724607dc936d66629183a480864befd64d59614
Nick Gronow <nick@webonlineventures.com>
2014-05-16 12:05:24 -0400

    Added controller tests

--- spec/controllers/article_spec.rb
+++ spec/controllers/article_spec.rb
@@ -8,4 +8,27 @@ describe ArticlesController do
       expect(assigns(:articles)).to include(article)
     end
   end
+
+  describe "GET new" do
+    it "makes a new article" do
+      get :new
+      expect(assigns(:article)).to_not be_nil
+    end
+  end
+
+  describe "POST create" do
+    it "Saves a new article" do
+      post :create, article: { title: 'My catchy title' }
+      Article.all.count.should == 1
+    end
+  end
+
+  describe "POST results" do
+    it "Display results" do
+      article = Article.create(title: 'My cool article')
+      Article.reindex
+      post :results, search: 'cool'
+      expect(assigns(:articles)).to include(article)
+    end
+  end
 end
